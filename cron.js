c3761554e76e4a4895087b0a9ef7d772a011cb52
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-04-07 15:42:16 +0200

    update cron freq

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron')
 const cp = require('child_process')
 
-cron.schedule('0 9,13,17,21 * * *', () => {
+cron.schedule('0 */1 * * *', () => {
   console.log('Running');
   cp.fork(__dirname + '/index.js')
 });
