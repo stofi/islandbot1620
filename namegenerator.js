ae99f699d32bcd3c5ac7c778483a8b68670e73fe
Kryštof Rainisch <krystof.rainisch@sprites.co>
2019-04-08 17:36:13 +0200

    add white space

--- namegenerator.js
+++ namegenerator.js
@@ -1,11 +1,10 @@
 const { one } = require('nouns')
+
+
 const capitalize = (s) => {
   if (typeof s !== 'string') return ''
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
-
-
-
 
 
 const nameGenerator = (options = {}) => {
